a42903192616c95666bebb0a7ff58dac180a2c54
VictorioCristiansaPutra <100137010+HyperTor29@users.noreply.github.com>
2023-05-27 16:55:25 +0700

    Add files via upload

---
+++ Pertemuan 8/Generics/BangunDatar.java
@@ -0,0 +1,8 @@
+/**
+ * Penulis : Victorio Cristiansa Putra 19/05/2023
+ * File : BangunDatar.java
+ * Deskripsi : kelas abstrak untuk bangun datar
+ */
+public abstract class BangunDatar {
+    public abstract double hitungkeliling();
+}
